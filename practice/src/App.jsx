d211cabf271aa04ef8f06db7822f6bed163cb48b
yakov504 <yakov0546080824@gmail.com>
2024-12-31 10:35:06 +0200

    useState practice

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { useState } from 'react';
 
 function App() {
+
+  const [ advice, setAdvice ] = useState('let get started')
+  const [ count, setCount ] = useState(0)
+
+  async function getAdvice() {
+    
+    const data = 'And click ...';
+    setCount ((c) => c + 1 );
+    setAdvice(data);
+
+  }
+
   return (
     <div>
-      
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get advice</button>
+      <p>you are clicked {count} times</p>
     </div>
   )
 }
 
 export default App;
+
 
